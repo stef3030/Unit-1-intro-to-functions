e3bb486e5c9895572536e6fbd3387d8de6c6fb86
stef3030 <Stefsantana3030@gmail.com>
2026-09-14 10:21:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+
+
+t.shape('turtle')
+
+t.forward(200)
+
+def message(input):
+    print(input)
+message("Hello Class")
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+    t.backward(50)
+turtle.done()
