eca9a0c60160d0cda650f5f61931b242d9e86159
stef3030 <Stefsantana3030@gmail.com>
2026-09-17 09:39:42 -0400

    gjfhj

---
+++ #defining a function.py
@@ -0,0 +1,9 @@
+#defining a function
+def add(x,y)
+    #vars created inside a function only exist inside that function
+    #print (x,y)
+    return x + y
+#call the function/invoke the function
+print (add(5,15))
+z = add(5,15)
+print (z) 
--- app.py
+++ app.py
@@ -6,11 +6,7 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
-
-def message(input):
-    print(input)
-message("Hello Class")
+""" t.forward(200)
 def square(x):
     t.forward(x)
     t.left(90)
@@ -21,5 +17,39 @@ def square(x):
     t.forward(x)
     t.left(90)
 square(200)
-    t.backward(50)
+
+
+
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200)  """
+""" 
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+def rectangle (x,y):
+    def (x)
+    def (y)
+    t.forward(x)
+    t.left(100)
+    t.forward(y)
+    t.left(125)
+    t.forward(x)
+    t.left(100)
+    t.forward(y)
+    t.left(125)
+rectangle (x,y)
+
+
 turtle.done()
