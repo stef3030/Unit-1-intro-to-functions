6bc9faf9ca89e69814f9b227eed214596926dd20
stef3030 <Stefsantana3030@gmail.com>
2026-09-17 10:31:44 -0400

    ...

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import turtle
 from turtle import *
 t = Turtle()
 
-
-
 t.shape('turtle')
 
 """ t.forward(200)
@@ -37,10 +35,9 @@ def right():
     t.left(135)
     t.forward(142)
 right() """
-
+""" 
 def rectangle (x,y):
-    def (x)
-    def (y)
+    
     t.forward(x)
     t.left(100)
     t.forward(y)
@@ -49,7 +46,43 @@ def rectangle (x,y):
     t.left(100)
     t.forward(y)
     t.left(125)
-rectangle (x,y)
+rectangle (x,y)""" 
+ 
+""" for i in range(3):
+    print(i)
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+sidelength = 100
+rotate = 90
+""" """
+"""
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+""" """ square(100,90)  """
+""" def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5) """
+ """
+ 
+def addSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 5
+        t.right (5) """
+ addSquares(60) 
 
+def square (x,y)
+    for i in range (60)
+    square(x,y)
+    t.right (5)
+    square (100,90)
 
-turtle.done()
+ 
+turtle.done() 
